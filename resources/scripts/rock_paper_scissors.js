788f4a4ac27b26e09c1ddc1d103555cdcf38e779
Taka Kiuchi <104165510+TakaKiu@users.noreply.github.com>
2024-06-02 18:52:32 -0400

    Update rock_paper_scissors.js

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -1,47 +1,44 @@
 class RockPaperScissors {
-  constructor(username) {
-    this.username = username;
-    this.score = {
-      user: 0,
-      cpu:0 
-    },
-    this.gameHistoryLog = [];
-  }
+    constructor(username) {
+        this.username = username;
+        this.score = {
+            user: 0,
+            cpu: 0
+        };
+        this.gameHistoryLog = [];
+    }
 
-  /**
-   * RETURN: one of the following values (`rock`, `paper`, `scissors`)
-   * using Math.random() method, you should be able to get one of the following values
-   */
-  generateCPUResponse(){
-    const acceptedValues = [ `rock`, `paper`, `scissors` ];
+    generateCPUResponse() {
+        const acceptedValues = ['rock', 'paper', 'scissors'];
+        const randomIndex = Math.floor(Math.random() * acceptedValues.length);
+        return acceptedValues[randomIndex];
+    }
 
-    return ;
-  }
-  /**
-   * returns one of the following values: `win`, `lose`, `tie`
-   * tie:
-   *     the user selection the same as the CPU
-   * win: 
-   *    (user is `rock` and cpu is `scissors
-   *     OR
-   *    (user is `paper` and cpu is `rock`) 
-   *     OR 
-   *    (user is `scissors` and cpu is `paper`)
-   * `lose`:
-   *    the opposite case :)
-   * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   * @param {string} cpuSelection computer selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   */
-  determineWinner(userSelection, cpuSelection){
+    determineWinner(userSelection, cpuSelection) {
+        if (userSelection === cpuSelection) {
+            return 'tie';
+        } else if (
+            (userSelection === 'rock' && cpuSelection === 'scissors') ||
+            (userSelection === 'paper' && cpuSelection === 'rock') ||
+            (userSelection === 'scissors' && cpuSelection === 'paper')
+        ) {
+            return 'win';
+        } else {
+            return 'lose';
+        }
+    }
 
-  }
+    play(userSelection) {
+        const cpuSelection = this.generateCPUResponse();
+        const result = this.determineWinner(userSelection, cpuSelection);
 
-  /**
-   * 
-   * @param {string} userSelection user selection. Can only be one of the following values [`rock`, `paper`, `scissors`]
-   */
-  play(userSelection){
+        if (result === 'win') {
+            this.score.user++;
+        } else if (result === 'lose') {
+            this.score.cpu++;
+        }
 
-  }
-
+        const logEntry = `${this.username} selected ${userSelection}, CPU selected ${cpuSelection}: ${this.username} ${result}s`;
+        this.gameHistoryLog.push(logEntry);
+    }
 }
