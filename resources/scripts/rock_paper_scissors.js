aa2225dcaecd969a66f712219516e43ad3b6181f
Taka Kiuchi <104165510+TakaKiu@users.noreply.github.com>
2024-07-07 15:38:47 -0400

    Update rock_paper_scissors.js

--- resources/scripts/rock_paper_scissors.js
+++ resources/scripts/rock_paper_scissors.js
@@ -1,44 +1,57 @@
+
 class RockPaperScissors {
-    constructor(username) {
-        this.username = username;
-        this.score = {
-            user: 0,
-            cpu: 0
-        };
-        this.gameHistoryLog = [];
+  constructor(username) {
+    this.username = username;
+    this.score = { user: 0, cpu: 0 };
+    this.gameHistoryLog = [];
+  }
+
+  generateCPUResponse() {
+    const options = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+
+  determineWinner(userSelection, cpuSelection) {
+    if (userSelection === cpuSelection) {
+      return 'tie';
+    } else if (
+      (userSelection === 'rock' && cpuSelection === 'scissors') ||
+      (userSelection === 'paper' && cpuSelection === 'rock') ||
+      (userSelection === 'scissors' && cpuSelection === 'paper')
+    ) {
+      return 'win';
+    } else {
+      return 'lose';
+    }
+  }
+
+  play(userSelection) {
+    const cpuSelection = this.generateCPUResponse();
+    const result = this.determineWinner(userSelection, cpuSelection);
+
+    if (result === 'win') {
+      this.score.user++;
+    } else if (result === 'lose') {
+      this.score.cpu++;
     }
 
-    generateCPUResponse() {
-        const acceptedValues = ['rock', 'paper', 'scissors'];
-        const randomIndex = Math.floor(Math.random() * acceptedValues.length);
-        return acceptedValues[randomIndex];
-    }
+    const logEntry = `${this.username} selected ${userSelection}, CPU selected ${cpuSelection}: ${this.username} ${result}s`;
+    this.gameHistoryLog.push(logEntry);
 
-    determineWinner(userSelection, cpuSelection) {
-        if (userSelection === cpuSelection) {
-            return 'tie';
-        } else if (
-            (userSelection === 'rock' && cpuSelection === 'scissors') ||
-            (userSelection === 'paper' && cpuSelection === 'rock') ||
-            (userSelection === 'scissors' && cpuSelection === 'paper')
-        ) {
-            return 'win';
-        } else {
-            return 'lose';
-        }
-    }
+    return {
+      result,
+      userSelection,
+      cpuSelection,
+      score: { ...this.score },
+      logEntry,
+    };
+  }
 
-    play(userSelection) {
-        const cpuSelection = this.generateCPUResponse();
-        const result = this.determineWinner(userSelection, cpuSelection);
-
-        if (result === 'win') {
-            this.score.user++;
-        } else if (result === 'lose') {
-            this.score.cpu++;
-        }
-
-        const logEntry = `${this.username} selected ${userSelection}, CPU selected ${cpuSelection}: ${this.username} ${result}s`;
-        this.gameHistoryLog.push(logEntry);
-    }
+  resetGame() {
+    this.score = { user: 0, cpu: 0 };
+    this.gameHistoryLog = [];
+  }
 }
+
+export default RockPaperScissors;
